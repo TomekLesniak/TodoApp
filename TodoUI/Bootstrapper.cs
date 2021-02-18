68e89b785c9faebb5a0ba9e9972c02a19832820b
TomekLesniak <tomasz.lesniak152@gmail.com>
2021-02-18 13:15:04 +0100

    Configure custom dependency-injection system
    Created custom dependency-injection system to work with caliburn micro & entity framework core

---
+++ TodoUI/Bootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Caliburn.Micro;
+using TodoUI.ViewModels;
+
+namespace TodoUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void Configure()
+        {
+            _container.Singleton<IWindowManager, WindowManager>();
+            _container.Singleton<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ TodoUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Caliburn.Micro;
+
+namespace TodoUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+    }
+}
